ea8f917199923cfd093d29a996b5b7ebb8986df5
Francieli Viane <francielifrv@gmail.com>
2016-12-30 17:20:47 -0200

    Returns buzz for number 5

--- src/FizzBuzz.hs
+++ src/FizzBuzz.hs
@@ -3,4 +3,5 @@ module FizzBuzz where
 fizzBuzz :: Integer -> String
 fizzBuzz number
   | number == 3  = "fizz"
+  | number == 5  = "buzz"
   | otherwise       = show number
--- test/FizzBuzzSpec.hs
+++ test/FizzBuzzSpec.hs
@@ -4,8 +4,11 @@ import FizzBuzz
 main :: IO ()
 main = hspec $ do
   describe "fizz buzz" $ do
-    it "returns the original number" $
+    it "returns the original number when it is not divisible by 3 or/and 5" $
       fizzBuzz (1) `shouldBe` "1"
 
     it "returns fizz when number is 3" $ do
       fizzBuzz(3) `shouldBe` "fizz"
+
+    it "returns fizz when number is 5" $ do
+      fizzBuzz(5) `shouldBe` "buzz"
